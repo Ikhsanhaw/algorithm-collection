907e4f37bba826453cda91d60f7c29dec5b1b241
fgarnadi <fajar.garnadi@gmail.com>
2019-10-31 11:44:22 +0700

    longest substring without repeating

---
+++ python/longest_substring_without_repeating.py
@@ -0,0 +1,13 @@
+def lengthOfLongestSubstring(self, s):
+    lookup_ = set()
+    max_ = 0
+    length = 0
+    for c in s:
+        if c not in lookup_:
+            length = length + 1
+            lookup_.add(c)
+        else:
+            max_ = max(length, max_)
+            length = 0
+            lookup_ = set()
+    return max_
